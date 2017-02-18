dff7fcf2f29444b44408f88b0b62be4cb8e7dc60
ashishgulati71 <ashishgulati71@gmail.com>
2017-02-19 02:58:05 +0530

    first commit / hope it works

---
+++ Assets/WebcamToPNG.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WebcamToPNG : MonoBehaviour
+{
+	public GameObject m_screen;
+
+	private WebCamTexture m_WebCamTexture;
+	private string _frontCam;
+	private string _backCam;
+
+	void Awake(){
+
+
+
+		WebCamDevice[] devices = WebCamTexture.devices;    // Return a list of available devices
+
+		for(var i = 0; i < devices.Length; i++){
+			if(devices[i].isFrontFacing){
+				_backCam = devices [i].name;
+				//m_WebCamTexture.deviceName = _frontCam;
+			}else{
+				_backCam = devices[i].name;
+				//m_WebCamTexture.deviceName = _backCam;
+			}
+		}
+
+		m_WebCamTexture = new WebCamTexture (_backCam, 640, 480, 60);
+		m_WebCamTexture.Play();	
+
+		m_screen.GetComponent<Renderer> ().material.mainTexture = m_WebCamTexture;
+
+
+		if(Application.isMobilePlatform){
+			GameObject cameraParent =new GameObject ("camParent");
+			cameraParent.transform.position=this.transform.position;
+			this.transform.parent=cameraParent.transform;
+			cameraParent.transform.Rotate(Vector3.right,90);
+		}
+		Input.gyro.enabled=true;
+	}
+
+	void Update () {
+		Quaternion cameraRoation = new Quaternion(Input.gyro.attitude.x,Input.gyro.attitude.y,-Input.gyro.attitude.z,-Input.gyro.attitude.w);
+		Debug.Log(Input.gyro.attitude.x+" "+Input.gyro.attitude.y+" "+Input.gyro.attitude.z+" "+Input.gyro.attitude.w);
+
+		this.transform.localRotation=cameraRoation;
+		Debug.Log( "as"+cameraRoation);
+	}	
+}
---
+++ Assets/webcamtext.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class webcamtext : MonoBehaviour {
+	public GameObject webplane;
+	float yRotation;
+	float xRotation;
+	// Use this for initialization
+	void Start () {
+
+		if(Application.isMobilePlatform){
+			GameObject cameraParent =new GameObject ("camParent");
+			cameraParent.transform.position=this.transform.position;
+			this.transform.parent=cameraParent.transform;
+			cameraParent.transform.Rotate(Vector3.right,90);
+		}
+		Input.gyro.enabled=true;
+
+
+		WebCamTexture wt= new WebCamTexture();
+		webplane.GetComponent<MeshRenderer>().material.mainTexture=wt;
+		wt.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion cameraRoation = new Quaternion(Input.gyro.rotationRateUnbiased.x,Input.gyro.rotationRateUnbiased.y,-Input.gyro.rotationRateUnbiased.z,0);
+		Debug.Log(Input.gyro.rotationRateUnbiased.x+" "+Input.gyro.rotationRateUnbiased.y+" "+Input.gyro.rotationRateUnbiased.z+" ");
+	//	Debug.Log(Input.gyro.rotationRateUnbiased.x);
+		//this.transform.rotation = Input.gyro.attitude;
+		this.transform.localRotation=cameraRoation;
+		//Debug.Log( "as"+cameraRoation);
+		/*
+
+		yRotation += -Input.gyro.rotationRateUnbiased.y;
+		xRotation += -Input.gyro.rotationRateUnbiased.x;
+		transform.eulerAngles = new Vector3(xRotation, yRotation, 0);
+*/
+
+	}
+}
